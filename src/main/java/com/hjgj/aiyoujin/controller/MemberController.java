e7acfd259e561d7da0d6d53b9f5c37a3563ca45e
mayingdong <mayingdong@32gold.com>
2018-01-08 11:21:55 +0800

    修改

--- src/main/java/com/hjgj/aiyoujin/controller/MemberController.java
+++ src/main/java/com/hjgj/aiyoujin/controller/MemberController.java
@@ -26,7 +26,7 @@ public class MemberController extends BaseController {
 
 	
 
-
+	//
 	@RequestMapping(value = "addAddress", method = RequestMethod.POST)
 	public ResultModel addAddress(String userId, 
 			@RequestParam String contact,
