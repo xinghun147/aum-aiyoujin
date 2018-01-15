550dc04286479411b17bf6cbd612291e886dba67
xiaojian <sunxiaojian@32gold.com>
2018-01-15 14:39:26 +0800

    微信模板通知

--- aiyoujin-server/src/main/java/com/hjgj/aiyoujin/ServerApplication.java
+++ aiyoujin-server/src/main/java/com/hjgj/aiyoujin/ServerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 
