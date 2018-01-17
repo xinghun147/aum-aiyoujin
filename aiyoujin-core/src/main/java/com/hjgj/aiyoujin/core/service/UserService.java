2e60ba9ff17197efe3640132e1abb35fddf7bf97
ailiming <ailiming@32gold.com>
2018-01-17 14:21:05 +0800

    fix

--- aiyoujin-core/src/main/java/com/hjgj/aiyoujin/core/service/UserService.java
+++ aiyoujin-core/src/main/java/com/hjgj/aiyoujin/core/service/UserService.java
@@ -1,5 +1,14 @@
 package com.hjgj.aiyoujin.core.service;
 
+import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.ibatis.session.RowBounds;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.hjgj.aiyoujin.core.common.Constants;
 import com.hjgj.aiyoujin.core.common.utils.UUIDGenerator;
 import com.hjgj.aiyoujin.core.dao.UserMapper;
@@ -7,12 +16,6 @@ import com.hjgj.aiyoujin.core.model.User;
 import com.hjgj.aiyoujin.core.model.UserExample;
 import com.hjgj.aiyoujin.core.model.vo.Page;
 import com.hjgj.aiyoujin.core.model.vo.UserVO;
-import org.apache.ibatis.session.RowBounds;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.Date;
-import java.util.List;
 
 @Service
 public class UserService {
@@ -39,13 +42,30 @@ public class UserService {
         }
         return page;
     }
-
+    
+    @Transactional
     public User insertUser(User user) throws Exception {
-        user.setId(UUIDGenerator.generate());
-        user.setCreateTime(new Date());
-        user.setDeleted(Constants.DelFlag.NO.ordinal());
-        user.setUpdateTime(new Date());
-        userMapper.insert(user);
+	    UserExample example = new UserExample();
+        UserExample.Criteria criteria = example.createCriteria();
+        criteria.andOpenIdEqualTo(user.getOpenId());
+    	User currUser = userMapper.selectOneByExample(example);
+    	if(currUser != null){
+    		currUser.setUpdateTime(new Date());
+    		if(StringUtils.isNotBlank(user.getAvatar())){
+    			currUser.setAvatar(user.getAvatar());
+    		}
+    		if(StringUtils.isNotBlank(user.getNickname())){
+    			currUser.setNickname(user.getNickname());
+    		}
+    	    userMapper.updateByPrimaryKeySelective(currUser);
+    	    return currUser;
+    	}else{
+    		user.setId(UUIDGenerator.generate());
+    		user.setCreateTime(new Date());
+    		user.setDeleted(Constants.DelFlag.NO.ordinal());
+    		user.setUpdateTime(new Date());
+    		userMapper.insert(user);
+    	}
         return user;
     }
 
