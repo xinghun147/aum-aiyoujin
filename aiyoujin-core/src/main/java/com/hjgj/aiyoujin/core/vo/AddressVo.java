14b4ce1caf8cb3ec2136d290277b43b188cc4b4a
xiaojian <sunxiaojian@32gold.com>
2018-02-02 17:33:41 +0800

    修改地址的vo的vo

--- aiyoujin-core/src/main/java/com/hjgj/aiyoujin/core/vo/AddressVo.java
+++ aiyoujin-core/src/main/java/com/hjgj/aiyoujin/core/vo/AddressVo.java
@@ -1,117 +1,124 @@
 package com.hjgj.aiyoujin.core.vo;
 
 
-
 import java.io.Serializable;
 
 public class AddressVo implements Serializable {
-	
-	private static final long serialVersionUID = 1L;
-	private String id;
-	private String contact;
-	private String telephone;
-	private String areaId;
-	private String address;
-	private String postCode;
-	private String userId;
-	private Integer isDefault;
-	private Integer deleted;
-	
 
-	public AddressVo() {
-		super();
-	}
+    private static final long serialVersionUID = 1L;
+    private String id;
+    private String contact;
+    private String telephone;
+    private String areaId;
+    private String address;
+    private String postCode;
+    private String userId;
+    private Integer isDefault;
+    private Integer deleted;
+    private String userNickName;
 
-	public AddressVo(String id, Integer isDefault) {
-		super();
-		this.id = id;
-		this.isDefault = isDefault;
-	}
-	
+    public AddressVo() {
+        super();
+    }
 
-	public AddressVo(String id, String contact, String telephone, String areaId, String address, String postCode,
-			String userId, Integer isDefault, Integer deleted) {
-		super();
-		this.id = id;
-		this.contact = contact;
-		this.telephone = telephone;
-		this.areaId = areaId;
-		this.address = address;
-		this.postCode = postCode;
-		this.userId = userId;
-		this.isDefault = isDefault;
-		this.deleted = deleted;
-	}
+    public AddressVo(String id, Integer isDefault) {
+        super();
+        this.id = id;
+        this.isDefault = isDefault;
+    }
 
-	public String getUserId() {
-		return userId;
-	}
 
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
+    public AddressVo(String id, String contact, String telephone, String areaId, String address, String postCode,
+                     String userId, Integer isDefault, Integer deleted) {
+        super();
+        this.id = id;
+        this.contact = contact;
+        this.telephone = telephone;
+        this.areaId = areaId;
+        this.address = address;
+        this.postCode = postCode;
+        this.userId = userId;
+        this.isDefault = isDefault;
+        this.deleted = deleted;
+    }
 
-	public Integer getIsDefault() {
-		return isDefault;
-	}
+    public String getUserId() {
+        return userId;
+    }
 
-	public void setIsDefault(Integer isDefault) {
-		this.isDefault = isDefault;
-	}
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 
-	public Integer getDeleted() {
-		return deleted;
-	}
+    public Integer getIsDefault() {
+        return isDefault;
+    }
 
-	public void setDeleted(Integer deleted) {
-		this.deleted = deleted;
-	}
+    public void setIsDefault(Integer isDefault) {
+        this.isDefault = isDefault;
+    }
 
-	public String getId() {
-		return id;
-	}
+    public Integer getDeleted() {
+        return deleted;
+    }
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public void setDeleted(Integer deleted) {
+        this.deleted = deleted;
+    }
 
-	public String getContact() {
-		return contact;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public void setContact(String contact) {
-		this.contact = contact;
-	}
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	public String getTelephone() {
-		return telephone;
-	}
+    public String getContact() {
+        return contact;
+    }
 
-	public void setTelephone(String telephone) {
-		this.telephone = telephone;
-	}
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
 
-	public String getAreaId() {
-		return areaId;
-	}
+    public String getTelephone() {
+        return telephone;
+    }
 
-	public void setAreaId(String areaId) {
-		this.areaId = areaId;
-	}
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 
-	public String getAddress() {
-		return address;
-	}
+    public String getAreaId() {
+        return areaId;
+    }
 
-	public void setAddress(String address) {
-		this.address = address;
-	}
+    public void setAreaId(String areaId) {
+        this.areaId = areaId;
+    }
 
-	public String getPostCode() {
-		return postCode;
-	}
+    public String getAddress() {
+        return address;
+    }
 
-	public void setPostCode(String postCode) {
-		this.postCode = postCode;
-	}
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    public String getUserNickName() {
+        return userNickName;
+    }
+
+    public void setUserNickName(String userNickName) {
+        this.userNickName = userNickName;
+    }
 }
